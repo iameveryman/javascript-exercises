b896df78163483dfdba2bd25fac9913a2e2545ff
Eric Andrews <50085784+iameveryman@users.noreply.github.com>
2025-01-01 10:51:55 -0500

    Commit for completion of first two exercises
    This is the commit for solutions to the helloWorld
    and repeatString exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeatedStr = ''; //set variable to contain output
+    if (number < 0){ 
+        return 'ERROR';
+    } //condition to weed out negative numbers
+    for (let i = 0; i < number; i++){
+        repeatedStr += string;
+    }
+    return repeatedStr;
 };
+
+repeatString('hey', 3); //test 1
+repeatString('hello', 10); //test 2
+repeatString('hi', 1); //test 3
+repeatString('bye', 0); //test 4
+repeatString('goodby', -1);  //test 5
+repeatString('odin', ); //test 6
+repeatString('', 10); //test 7
 
 // Do not edit below this line
 module.exports = repeatString;
